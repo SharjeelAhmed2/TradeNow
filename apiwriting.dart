64f23fcebecec12b03197e2c276b980e5e4f184d
SharjeelAhmed2 <71289420+SharjeelAhmed2@users.noreply.github.com>
2020-10-06 09:14:03 -0700

    contains the api get post methods

---
+++ apiwriting.dart
@@ -0,0 +1,33 @@
+import 'package:Project/apilearnwork/userinsertdata.dart';
+import 'package:Project/apiwork/api_response.dart';
+import 'package:Project/apilearnwork/userinsertdata.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiDatas
+{
+  static const api = "URL";
+  static const headers = {
+    'apiKey' : 'APIKEy',
+    'Content-Type': 'application/josn'
+  };  
+  //SignUp
+   Future<APIResponse<bool>> createUser(UserData item) {
+     return http.post(api + '/tradenow', headers: headers, body: json.encode(item.toJson())).then((data) {
+       if (data.statusCode == 201)
+       {
+         return APIResponse<bool>(data: true);
+
+       }
+       return APIResponse<bool>(error: true);
+     })
+     .catchError((_)=> APIResponse<bool>(error: true));
+   }
+   //Login
+    Future loginUser(String username, String password) async  
+   {
+     final response = await http.post(api, headers: headers, body: {'username' : username, 'password': password});
+     var convertedDatatoJson = jsonDecode(response.body);
+     return convertedDatatoJson;
+   }
+}
